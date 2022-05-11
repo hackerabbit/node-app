2f09f8fd9cd505d411eac54fc04e1def055acd17
yinliyun <1773737856@qq.com>
2022-05-11 17:30:43 +0800

    jwt auth

--- server.js
+++ server.js
@@ -2,11 +2,19 @@
  * @Author: hackrabbit
  * @Date: 2022-05-06 10:29:15
  * @LastEditors: hackrabbit
- * @LastEditTime: 2022-05-07 15:45:43
+ * @LastEditTime: 2022-05-11 16:43:04
  * @Description: 
  */
 const express = require('express');
 const multer = require('multer');
+
+// jwt 
+const jwt = require('jsonwebtoken');
+const { expressjwt: expressjwt } = require('express-jwt');
+
+const token = jwt.sign({ foo: 'bar' }, 'secret');
+console.log(token);
+
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
@@ -22,9 +30,46 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+
+app.use(expressjwt({
+  secret: 'jsreacttoken',
+  algorithms: ['HS256']
+}).unless({
+  path: ['/login', '/register']
+}))
+
+app.post('/login', (req, res) => {
+  console.log(req.body);
+  const { username, password } = req.body;
+  if (username === 'admin' && password === 'admin') {
+    res.json({
+      success: true,
+      token: jwt.sign({ username: username, password: password }, 'jsreacttoken')
+    });
+  } else {
+    res.json({
+      success: false,
+      message: '用户名或密码错误'
+    });
+  }
+})
+
+
 const { select, insert, deleteData } = require('./db/index');
+const req = require('express/lib/request');
 // select file
 app.get('/data', (req, res) => {
+  console.log(req.headers)
+
+  res.json({
+    code: 1,
+    data: {
+      list: [
+        { username: 'yly111', password: '123456' },
+      ]
+    }
+  })
+  return;
   select('select * from user', (err, results, fields) => {
     if (err) {
       res.send(err);
